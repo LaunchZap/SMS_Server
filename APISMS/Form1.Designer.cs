83fe6807f0487aa645abd06fcba376e52d587c24
Rene Josseph Fajardo Valdes <renejfajardo@gmail.com>
2014-11-17 20:42:05 -0500

    Primera versión estable de recepción de SMS y almacenados en DB.

---
+++ APISMS/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace APISMS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.bOpen = new System.Windows.Forms.Button();
+            this.bSend = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Estado = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.MySqlStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bOpen
+            // 
+            this.bOpen.Location = new System.Drawing.Point(415, 70);
+            this.bOpen.Name = "bOpen";
+            this.bOpen.Size = new System.Drawing.Size(75, 23);
+            this.bOpen.TabIndex = 1;
+            this.bOpen.Text = "Open";
+            this.bOpen.UseVisualStyleBackColor = true;
+            this.bOpen.Click += new System.EventHandler(this.bOpen_Click);
+            // 
+            // bSend
+            // 
+            this.bSend.Location = new System.Drawing.Point(415, 166);
+            this.bSend.Name = "bSend";
+            this.bSend.Size = new System.Drawing.Size(75, 23);
+            this.bSend.TabIndex = 3;
+            this.bSend.Text = "Send";
+            this.bSend.UseVisualStyleBackColor = true;
+            this.bSend.Click += new System.EventHandler(this.bSend_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(288, 70);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(270, 237);
+            this.textBox1.TabIndex = 20;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(289, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Estado:";
+            // 
+            // Estado
+            // 
+            this.Estado.AutoSize = true;
+            this.Estado.Location = new System.Drawing.Point(339, 98);
+            this.Estado.Name = "Estado";
+            this.Estado.Size = new System.Drawing.Size(0, 13);
+            this.Estado.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(292, 166);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(117, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(289, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "MySql Status:";
+            // 
+            // MySqlStatus
+            // 
+            this.MySqlStatus.AutoSize = true;
+            this.MySqlStatus.Location = new System.Drawing.Point(359, 120);
+            this.MySqlStatus.Name = "MySqlStatus";
+            this.MySqlStatus.Size = new System.Drawing.Size(13, 13);
+            this.MySqlStatus.TabIndex = 22;
+            this.MySqlStatus.Text = "_";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 261);
+            this.Controls.Add(this.MySqlStatus);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Estado);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.bSend);
+            this.Controls.Add(this.bOpen);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "apiSMS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button bOpen;
+        private System.Windows.Forms.Button bSend;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Estado;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label MySqlStatus;
+    }
+}
+
---
+++ APISMS/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Data.Odbc;
+using MySql.Data.MySqlClient;
+
+namespace APISMS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {                       
+            InitializeComponent();
+            findPorts();           
+        }
+
+        //public SerialPort serialPort;       
+
+        private void findPorts()
+	    {
+			// get port names
+		    //array<Object^>^ objectArray = SerialPort.GetPortNames();
+			// add string array to combobox
+			//this->comboBox1->Items->AddRange( objectArray );
+            //comboBox1.AutoCompleteSource = AutoCompleteSource.CustomSource;
+            //comboBox1.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            //AutoCompleteStringCollection Ports = new AutoCompleteStringCollection();
+            //Ports.Add(serialPort1.PortName);
+            //comboBox1.AutoCompleteCustomSource = Ports;
+            // Get a list of serial port names.
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                comboBox1.Items.Add(port);
+            }
+            
+		}
+
+        private void bOpen_Click(object sender, EventArgs e)
+        {
+            Object selectedItem = comboBox1.SelectedItem;
+            if (selectedItem != null)
+            {
+                //serialPort = new SerialPort(selectedItem.ToString(), 9600, Parity.None, 8, StopBits.One);
+
+                if (!serialPort1.IsOpen && selectedItem != null)
+                {
+                    //textBox1.AppendText("Conectando\n");
+
+                    try
+                    {
+                        serialPort1.PortName = selectedItem.ToString();
+                        serialPort1.BaudRate = 19200;// 19200;
+                        serialPort1.Parity = Parity.None;
+                        serialPort1.DataBits = 8;
+                        serialPort1.StopBits = StopBits.One;
+                        serialPort1.Open();
+                        Estado.Text = "Open";
+                        comboBox1.Enabled = false;
+                        bOpen.Text = "Close";
+                        this.serialPort1.DataReceived += new SerialDataReceivedEventHandler(this.sport_DataReceived);
+                    }
+                    catch (Exception ex) { MessageBox.Show(ex.ToString(), "Error"); }
+                }
+                else if (serialPort1.IsOpen && selectedItem != null)
+                {
+                    //textBox1.AppendText("Cerrado\n");
+                    serialPort1.Close();
+                    Estado.Text = "Close";
+                    comboBox1.Enabled = true;
+                    bOpen.Text = "Open";
+                }
+            }
+                   
+        }
+
+        private void bSend_Click(object sender, EventArgs e)
+        {   
+            /******** Pendiente el envío de SMS desde la implementacion  ********/
+            if (serialPort1.IsOpen) serialPort1.Write(textBox2.Text + "\r");
+            //this.SetText("\r\n +CMT: \"3113047400\",,\"14/10/19,10:01:53-20\"\r\nHello world!!! \r\n");            
+        }
+
+        private void sport_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            String dtn = dt.ToShortTimeString();
+            string texto = serialPort1.ReadExisting();
+            //textBox1.AppendText("[" + dtn + "] " + "Received: " + texto + "\n");
+            //this.textBox1.AppendText(texto.ToString());
+            this.SetText(texto);            
+        }
+
+        // This delegate enables asynchronous calls for setting
+        // the text property on a TextBox control.
+        delegate void SetTextCallback(string text);
+
+        private void SetText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.textBox1.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                if (text == "")
+                    this.textBox1.Clear();
+                else
+                    this.textBox1.AppendText(text);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //Estado.Text = "cambio";
+            int indice;
+            string Buf = textBox1.Text;
+            string Origen="";
+            string timeStamp = "";
+            string Msg = "";
+
+            char[] value = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+            char[] fecha = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+            char[] sms = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+
+            if (Buf.IndexOf("\r", 0) == 0 && Buf.IndexOf("\r", 1) > 0 && Buf.IndexOf("\r", 46) > 44)
+            {
+                //Estado.Text = "Ok";
+                indice = Buf.IndexOf("\"", 0);
+
+                //Extrae el numero celular
+                Buf.CopyTo(indice+1, value, 0, 10);                
+                for (var index = 0; index < 10; index++ )
+                {
+                    Origen += value[index];
+                }
+
+                indice = Buf.IndexOf("\"", indice+1);
+
+                //Extrae la Fecha y hora
+                indice = Buf.IndexOf("\"", indice + 1);
+
+                Buf.CopyTo(indice + 1, fecha, 0, 17);
+                for (var index = 0; index < 17; index++)
+                {                    
+                    if (fecha[index].Equals('/'))
+                        timeStamp += "-";
+                    else if (fecha[index].Equals(','))
+                        timeStamp += " ";
+                    else
+                        timeStamp += fecha[index];
+                }
+
+                indice = Buf.IndexOf("\"", indice + 1);
+                
+                //Extrae el mensaje
+                indice = Buf.IndexOf("\n", indice + 1);
+
+                Buf.CopyTo(indice + 1, sms, 0, 14);
+                for (var index = 0; index < 14; index++)
+                {
+                    Msg += sms[index];
+                }
+
+                //MessageBox.Show("Origen: " + Origen + "\nRecivido: " + timeStamp + "\nMensaje: " + Msg, "Contenido SMS");
+                this.SetText("");
+                //Inserta SMS en la DB
+                    Mysql(Origen, timeStamp, Msg);
+            }
+            
+        }
+
+        private void prueba()
+        {
+            string Buf = "\r\n +CMT: \"3113047400\",,\"14/10/19,10:01:53-20\"\r\nHello world!!! \r\n";
+            char[] value = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+
+            Estado.Text = Buf.IndexOf("\r", 0) + " " + Buf.IndexOf("\r", 1) + " " + Buf.IndexOf("\r", 46);
+
+            if (Buf.IndexOf("\r", 0) == 0 && Buf.IndexOf("\r", 1) > 0 && Buf.IndexOf("\r", 46) > 44)
+            {
+                //Estado.Text = "Ok";
+                Estado.Text = Buf.IndexOf("\"", Buf.IndexOf("\"", 0) + 1) + " " + " ";
+                Buf.CopyTo(Buf.IndexOf("\"", 0)+1, value, 0, 10);
+                value.ToString();
+            }
+        }
+
+       private void Mysql( string Origen, string timeStamp , string Msg)
+        {
+            try
+            {
+                MySqlStatus.Text = "Ready";
+                //Connection string for Connector/ODBC 3.51
+                string MyConString = "Server=localhost;" +
+                  "Database=test;" +
+                  "Port=3306;" +
+                  "uid=designer;" +
+                  "password=test2014";
+                
+                string Insert = "insert into sms(Origen, timeStamp, Msg) value (" + Origen + ",'" + timeStamp + "',\"" + Msg + "\");";
+                MySqlConnection MyConnection = new MySqlConnection(MyConString);
+                MySqlDataAdapter myData = new MySqlDataAdapter();
+                              
+                MySqlCommandBuilder cb = new MySqlCommandBuilder(myData);
+                MySqlDataReader myRd;                
+
+                //Inserta
+                MyConnection.Open();
+                MySqlStatus.Text = "Connect";
+                myData.SelectCommand = new MySqlCommand(Insert, MyConnection);
+                myRd = myData.SelectCommand.ExecuteReader();
+                while (myRd.Read()) { }
+                MyConnection.Close();
+                MySqlStatus.Text = "Close";
+                
+                //MessageBox.Show("\n !!! success, connected successfully !!!\n", "success");
+                
+            }
+            catch (Exception MyOdbcException) //Catch any ODBC exception ..
+            {
+                MySqlStatus.Text = "Error";
+                MessageBox.Show(MyOdbcException.Message, "ERROR");
+
+            }
+
+        }
+
+
+    }
+}
